4f9511d6f928439a11034ec37e0cf82f51fe2a5b
聊原 <34804181@qq.com>
2026-05-09 19:56:01 +0800

    init: add base files and train local model

--- train_model.py
+++ train_model.py
@@ -1,12 +1,19 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import joblib
 
-
+# 加载训练数据
 data = pd.read_csv('training_data.csv')
-model = LinearRegression().fit(data[['x']], data['y'])
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 
+# 训练线性回归模型
+model = LinearRegression()
+model.fit(X, y)
 
+# 保存模型文件
+joblib.dump(model, 'linear_model.pkl')
+
+# 将模型的系数和截距保存到文本文件中
 with open('linear_model.txt', 'w') as f:
-    f.write(f"Slope: {model.coef_[0]}, Intercept: {model.intercept_}")
-
-print("训练完成！linear_model.txt 已生成。")
+    f.write(f'Coefficients: {model.coef_}\nIntercept: {model.intercept_}\n')
